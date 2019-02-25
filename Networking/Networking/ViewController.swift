f93e3415a0d128c71b9f819fb01f9dcc427c0089
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-25 09:05:51 +0200

    init start

---
+++ Networking/Networking/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Networking
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+
+    @IBAction func getImagePressed(_ sender: Any) {
+    }
+    
+}
+
