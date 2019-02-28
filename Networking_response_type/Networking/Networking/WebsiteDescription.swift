9bfb730ec4da85ebcaaee269bc2279912a6a0f22
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-28 09:14:53 +0200

    response type

---
+++ Networking_response_type/Networking/Networking/WebsiteDescription.swift
@@ -0,0 +1,13 @@
+//
+//  WebsiteDescription.swift
+//  Networking
+
+
+import Foundation
+
+struct WebsiteDescription: Decodable {
+    
+    let websiteDescription: String?
+    let websiteName: String?
+    let courses: [Course]
+}
